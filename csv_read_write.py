73bcc2421de156025a6ed98adde9421760d7b145
odel <delgur@gmail.com>
2019-06-26 13:33:21 -0400

    started menu add for csv_read_write, wrote where() on dihedral_linking.py

--- csv_read_write.py
+++ csv_read_write.py
@@ -4,55 +4,89 @@
 import csv
 
 
-#
-#   read csv in 
-#   for line in csv run program
-#   
+from colorings import *
 
-
-#
-# this didn't work so i just put the functions in :/
-#
 
 from gauss_to_overstrand import *
+from gauss_to_signlist import *
+from matrix_to_rrematrix import *
+from rrematrix_to_colourlist import *
+from overstrand_to_matrix import *
 
-def ToReducedRowEchelonForm( M):
-    if not M: return M
-    lead = 0 # col index
-    rowCount = len(M) # num rows in M
-    columnCount = len(M[0]) # num col in M
-    for r in range(lead, rowCount): #r is row index
-        if lead >= columnCount:  #if col index reaches end, return column
-            return M
-        i = r #
-        while M[i][lead] == 0: #if the value is already 0, move to the next row
-            i += 1
-            if i == rowCount: #we reached the last row
-                i = r
-                lead += 1
-                if columnCount == lead: #we reached the last column
-                    return M
-        #print("r is"+str(r))
-        #print("i is"+str(i))
-        #print("lead is"+str(lead))
-        #print("M is"+str(M))
-        M[i],M[r] = M[r],M[i] #swap two different rows (reassigning two vars at once)
-        #print("M is swapped"+str(M))
-        lv = M[r][lead] 
-        if lv == 2: #if the lead value is two, double the row and take mod 3
-        #print("lead value is"+str(lv))
-            M[r] = [ (mrx*2)%3 for mrx in M[r]]
-       # print("M row r is divided by lv"+str(M))
-        for i in range(rowCount):
-            if i != r: #everything below the lead value will be taken care of in the step
-                lv = M[i][lead]
-                #print("lv is"+str(lv))
-               # print("you take"+str(M[i])+"and subtract"+str(M[r])+"times lv")
-                M[i] = [ (iv - lv*rv)%3 for rv,iv in zip(M[r],M[i])]
-              #  print("M is "+str(M))
-        lead += 1
-    return M
-"""
+#
+# Goal is just a streamline csv reading and writing.
+# only need to specify:
+#           file you are reading
+#           the function you are applying
+#           file you are writing
+#
+
+import math
+
+def menu():
+    """ 
+    """
+    print("(0) ")
+    print("(1) csv read and write")
+    print("(2) ")
+    print("(3) ")
+    print("(4) ")
+    print("(5) ")
+    print("(6) ")
+    print("(7) ")
+    print("(9) Quit")
+    print(" ")    
+    print("Enter your choice:")
+
+def main():
+    """ the main user-interaction loop 
+    """
+
+    while True:     # the user-interaction loop
+        menu()
+        uc = input( "Choose an option: " )
+
+        if uc == 9: #
+            break
+
+        elif uc == 0:  #
+            #L = input("Enter a new list: ")
+
+        elif uc == 1:  #
+            csv_read_write()
+
+        elif uc == 2: # 
+            print(" ")
+
+        elif uc == 3: # 
+            print(" ")
+
+        elif uc == 4:
+            print(" ")
+
+        elif uc == 5:
+            print(" ")
+
+        elif uc == 6:
+            print(" ")
+
+        elif uc == 7:
+            print(" ")
+
+        else:
+            print "That's not on the menu!"
+
+    print(" ")
+    print(" ")
+    print(" exiting ")
+    print("         .")
+    print("           . ")
+    print("             . ")
+    print(" ")
+    print(" ")
+
+
+def 
 
 fields = [ 'Name', 'Coloring Matrix']
 
@@ -74,9 +108,9 @@ for row in reader:
         line_count = line_count + 1
     else:
         strings = row[1][2:-2].split("], [")
-        print(strings)
+        #print(strings)
         list_of_lists = [ list(crossing.split(",")) for crossing in strings ]
-        print(list_of_lists)
+        #print(list_of_lists)
         list_of_intlists = [ [int(n) for n in string] for string in list_of_lists ]
         new_LoL.append([row[0],ToReducedRowEchelonForm( list_of_intlists )])
         line_count = line_count + 1
@@ -85,7 +119,7 @@ rawfile.close()
 
 
 
-newfilename = "name_reducedmatrix1_3colorable.csv"
+newfilename = "name_reducedmatrix_3colorable.csv"
 
 newrawfile = open(newfilename, 'w')
 
@@ -96,42 +130,6 @@ for row in new_LoL:
 
 newrawfile.close()
 
-def create_dict(gauss_code_list):
-
-    strand_count = 0 # overstrand we are on
-    knot_dict = {}   # intialize knot dictionary
-    strand_indicies = [] # list of gauss code indicies that intersect a given overstrand
-    for i in range(len(gauss_code_list)): # go through entire gauss code
-        if gauss_code_list[i] < 0: # if we are at the understrand
-            strand_indicies.append(gauss_code_list[i])  # add index to list
-            knot_dict.update({strand_count:strand_indicies}) # update overstrand index dictionary
-            strand_count += 1 # go to the next strand
-            strand_indicies = [] # reset indicies
-        else:
-            strand_indicies.append(gauss_code_list[i]) # if we are an overstrand, add gauss code index
-                                                       # and continue
-    for i in reversed(range(len(gauss_code_list))):        
-        if gauss_code_list[i]>0: #if the last ones are positive, it's a part of strand 0
-            knot_dict[0].insert(0,gauss_code_list[i])
-        else: #not a part of strand 0
-            break
-    return knot_dict
-
-def create_overstrand_list(gauss_code_list):
-
-    gauss_code_dict = create_dict(gauss_code_list)
-    #print(gauss_code_dict)
-    overstrand_list = []
-    for key,val in gauss_code_dict.items():
-        lookingfor = val[-1]
-        #print("I'm looking for "+str(lookingfor)+" in the Gauss Code")
-        keys = [key for key, value in gauss_code_dict.items() if (-1)*lookingfor in value]
-        #print("The corresponding strand is "+str(keys))
-        #print(gauss_code_dict.items())
-        overstrand_list.append(keys[0])
-    if len(overstrand_list)%2 == 1:
-        overstrand_list.append(len(overstrand_list))
-    return overstrand_list
 """
 
 fields = [ 'Name', 'Gauss Code']
