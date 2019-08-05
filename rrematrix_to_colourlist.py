947508d1179e0146c2d71ac9e8c246865e9d30fa
gsambora <44476640+gsambora@users.noreply.github.com>
2019-08-05 16:31:13 -0400

    added some comments to rrematrix_to_colourlist

--- rrematrix_to_colourlist.py
+++ rrematrix_to_colourlist.py
@@ -1,7 +1,8 @@
 import csv
 
+#Makes a list of color lists that are valid for the knot, given the row reduced matrix for the strands. 
 def ColourList(matrix):
-    
+    #get number of rows as a variable and initialize lists for later. 
     number_rows = len(matrix)
 
     colourlists = []
@@ -10,21 +11,30 @@ def ColourList(matrix):
     free_variables = []
     free_var_names = []
 
-        
+    #Loop through each of the rows
     for i in range(number_rows):
+        #set a flag for when a pivot is in the row. 
         pivot_found = False
+        
+        #Loop through each of the columns within the row
         for j in range( len( matrix[i] ) ):
-            
+            #If we haven't found a pivot yet, keep going further right
             if pivot_found == False:
+                #If the value of this entry isn't 0, it's the pivot!
                 if matrix[i][j] > 0:
+                    #Add the location of the entry to our list pivots. 
                     pivot_location = j
                     pivots.append(pivot_location)
                     pivot_found = True
-
+                    
+                    
+    #Go through each of the variables (rows). 
+    #If the variable doesn't have a pivot, it is a free variable. Add to the list of free variables.
     for i in range(number_rows):
         if i not in pivots:
             free_variables.append(i)
-        
+    #jack do this :(   
+    #<|(:3)
     colour_dict ={}
     for k in range( len( free_variables ) ):
         colour_dict[free_variables[k]] = k%3
@@ -42,7 +52,7 @@ def ColourList(matrix):
         colours.append(colour_dict[i]+1)
 
     colourlists.append(colours)
-
+    
     if len(free_variables) == 3:
         free_colours = [[0,0,1],[0,1,0],[1,0,0]]
 
@@ -69,9 +79,11 @@ def ColourList(matrix):
         
     return colourlists
 
-
+#testing
 #print( ColourList([[1, 0, 0, 0, 0, 0, 0, 0, 1, 1], [0, 1, 0, 0, 0, 0, 0, 0, 0, 2], [0, 0, 1, 0, 0, 0, 0, 0, 2, 0], [0, 0, 0, 1, 0, 0, 0, 0, 0, 2], [0, 0, 0, 0, 1, 0, 0, 0, 1, 1], [0, 0, 0, 0, 0, 1, 0, 0, 1, 1], [0, 0, 0, 0, 0, 0, 1, 0, 2, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]))
 
+
+#Edit csv files. 
 fields = [ 'Name', 'Row Reduced Coloring Matrix']
 
 newfields = [ 'Name', 'Color List']
