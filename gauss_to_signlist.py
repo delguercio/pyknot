821b8f3ab5a8fa1a608b08302560662366bad96b
jackkkkkkkkkkkkkkk <51493405+jackkkkkkkkkkkkkkk@users.noreply.github.com>
2019-08-05 17:36:21 -0400

    Update gauss_to_signlist.py

--- gauss_to_signlist.py
+++ gauss_to_signlist.py
@@ -1,21 +1,20 @@
 # -----------------------------
-#    Filename: signlist.py
+#    Filename: gauss_to_signlist.py
 #      Author: Jack Kendrick
-# Last Update: 7/31/2019
+# Last Update: 8/5/2019
 # -----------------------------
 
 import csv
 
-
 def reverse_sect(values, start, end):
 
     """Reverses the section of the values list between the start and end points."""
-    
-    sect = values[start: end]
+   
+    sect = values[start: end]                   # Create a copy of the section that needs to be reversed
+    for i in range(len(sect)):                  # Removes values from the list
+        values.remove(sect[i])                  
     for i in range(len(sect)):
-        values.remove(sect[i])
-    for i in range(len(sect)):
-        values.insert(start, sect[i])
+        values.insert(start, sect[i])           # Places each value in the section at the start point, meaning the specified section is reversed
 
     return values
     
@@ -24,14 +23,14 @@ def kauffman(code):
 
     """Modifies the input Gauss code using Kauffman's algorithm."""
 
-    if type(code) == str:
+    if type(code) == str:                                   # If the code isn't already a list, this turns it into one
         code = code.split(", ")
     for i in range(len(code)):
         code[i] = int(code[i])
 
-    for crossing in range(1, len(code)//2+1):
-        for i in range(len(code)):
-            if abs(code[i]) == crossing:
+    for crossing in range(1, len(code)//2+1):               # Reverses the section of code between two elements in the Gauss code
+        for i in range(len(code)):                          # that have the same value, as outlined in Kauffman's Virtual Knot
+            if abs(code[i]) == crossing:                    # Theory (1996)
                 pair = FindPair(code, i)
                 code = reverse_sect(code, i+1, pair)
                 
@@ -53,21 +52,21 @@ def Rank(code):
 
     """Determines whether each arc around a modified Gauss code is a maximum (above the code) or a minimum (below the code). Also builds arc dictionary describing the start and end points of each arc in the modified Gauss code. Returns list of maximums, minimums, and the arc dictionary."""
 
-    max_list = [abs(code[0])]
-    min_list = []
-    found = [abs(code[0])]
-
-    n = len(code)
-    arcs = {}
+    max_list = [abs(code[0])]                                       # The first entry in the code is made a maximum arbitrarily.
+    min_list = []                                                   # Whether the first entry is made a maximum or a minimum
+    found = [abs(code[0])]                                          # determines whether the sign list is found for the knot or its
+                                                                    # mirror image. As this choice is currently arbitrary, the sign list
+    n = len(code)                                                   # could be for either the knot or its mirror image, meaning the
+    arcs = {}                                                       # dihedral linking number is currently found up to absolute value.   
 
     for i in range(n):
-        if code[i] > 0:
-            arc = code[i]
+        if code[i] > 0:                                             # Creates a dictionary of the arc number with its corresponding start
+            arc = code[i]                                           # and end point for use later.
             start = i
             end = FindPair(code, i)
             if end < 0:
                 end += n
-            arcs[arc] = [min(start, end), max(start, end)]
+            arcs[arc] = [min(start, end), max(start, end)]          # Start point is smaller by convention.
 
     for arc in found:
         found_inside = []
@@ -78,22 +77,22 @@ def Rank(code):
         start = arcs[arc][0]
         end = arcs[arc][1]
 
-        for value in code[start:end]:
+        for value in code[start:end]:                                   # Goes through the code inside the current arc.
             value = abs(value)
             if value not in found:
-                if arcs[value][0] < start or arcs[value][1]> end:
-                    if maximum:
-                        min_list.append(value)
+                if arcs[value][0] < start or arcs[value][1]> end:       # If one end of the inner arc is outside the current arcs,
+                    if maximum:                                         # it is a minimum if the current arc is a maximum and a
+                        min_list.append(value)                          # maximum if the current arc is a minimum.
                     else:
                         max_list.append(value)
 
                     found.append(value)
                     found_inside.append(value)
 
-        if len(found_inside) == 0 and found[-1] == arc and len(found)<n//2:
-            declared = True
-            while not declared:
-                for value in code[start:end]:
+        if len(found_inside) == 0 and found[-1] == arc and len(found)<n//2:  # If not all of the arcs have been classified as maximums
+            declared = True                                                  # or minimums but the program has already run through all of
+            while not declared:                                              # the found arcs, the next unclassified arc is made a maximum
+                for value in code[start:end]:                                # arbitrarily.
                     value = abs(value)
                     if value not in found:
                         max_list.append(value)
@@ -274,3 +273,12 @@ for row in new_LoL:
     writer.writerow(row)
 
 newrawfile.close()
+
+
+# -----------------------------
+# REFERENCES:
+# LH Kauffman, Virtual Knot Theory, European Journal of Combinatorics, 20 (1999), 663-691
+# -----------------------------
+
+
+
