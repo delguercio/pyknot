fe3a96655984f0537e3476899c5cfd011fc256a8
Jack Kendrick <jkendrick@smith.edu>
2019-10-07 13:52:07 -0400

    Fixed Gauss codes from Braid

--- BraidToSigns.py
+++ BraidToSigns.py
@@ -1,41 +1,51 @@
 def BraidToGauss(braid):
         # Creates a Gauss code from the braid notation.
-	gauss = []
-	i = 0
-	currentstrand = 0
-        
-	while len(gauss) < 2*len(braid):
-		index = i % len(braid)
-		crossing = braid[index]
+        gauss = []
+        i = 0
+        currentstrand = 0
 
-		if abs(crossing) == currentstrand:
-			currentstrand -= 1
-			if crossing > 0:
-				gauss.append(-(index + 1)) # +1 so no zero entry: no distinction between +0 and -0
-			else:
-				gauss.append(index + 1)
-		elif abs(crossing) == currentstrand + 1:
-			currentstrand += 1
-			if crossing > 0:
-				gauss.append(index + 1)
-			else:
-				gauss.append(-(index + 1))
+        crossings = {}
+        for i in range(len(braid)):
+                crossings[i] = 0
+                
+        count = 1
+        i = 0
+        while len(gauss) < 2*len(braid):
+                index = i % len(braid)
+                crossing = braid[index]
+                if abs(crossing)== currentstrand or abs(crossing)== currentstrand + 1:
+                        if crossings[index] == 0:
+                                crossings[index] = count
+                                count += 1
+                
+                if abs(crossing) == currentstrand:
+                        currentstrand -= 1
+                        if crossing > 0:
+                                gauss.append(-crossings[index])
+                        else:
+                                gauss.append(crossings[index])
+                elif abs(crossing) == currentstrand + 1:
+                        currentstrand += 1
+                        if crossing > 0:
+                                gauss.append(crossings[index])
+                        else:
+                                gauss.append(-crossings[index])
+                else:
+                        move = False
+                i += 1
 
-		i += 1
-
-	return gauss
-
+        return gauss
 
 def InitialSigns(braid):
         # Takes signs from braid notation: positive entry in braid notation = positive crossing
-	signs = []
+        signs = []
 
-	for crossing in braid:
-		if crossing < 0:
-			signs.append(-1)
-		else:
-			signs.append(1)
-	return signs
+        for crossing in braid:
+                if crossing < 0:
+                        signs.append(-1)
+                else:
+                        signs.append(1)
+        return signs
 
 def GaussToSigns(initialsigns, gauss):
         # Reorders sign list based on Gauss code
@@ -53,26 +63,4 @@ def BraidToSigns(braid):
         signs = GaussToSigns(initsigns, gauss)
 
         return signs
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
