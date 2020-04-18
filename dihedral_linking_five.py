be27eabc435ee7b8b9c82dbd4453fb6b7e2a20cc
odel <delgur@gmail.com>
2020-04-17 20:03:59 -0700

    back to work

--- dihedral_linking_five.py
+++ dihedral_linking_five.py
@@ -31,84 +31,49 @@ def reflect( current_universe , wall_color , p ):
     """
     return (2*wall_color - current_universe)%p 
 
-def side( list_of_lists ):
-    """
-    input:
-    output:
-    """
-    return
-
-def tier():
-    """
-    input:
-    output:
-    """
-    return
-
-# print("should be 1")
-# print("i got "+str(reflect(4,0)))
-# print("should be 2")
-# print("i got "+str(reflect(1,4)))
-# print("should be 3")
-# print("i got "+str(reflect(3,3)))
-
 
 def reflect_list( wall_1side_list , inout_strands , inout_strands_index , p ):
-    if len(wall_1side_list)==((p-1)/2)+1: # base case 
-        #print("inside base case")
-        #print(wall_1side_list)
-        return wall_1side_list
+    """
+    this function works by appending 
+
+    you initialize with inout_strands_index as either zero or one
+
+    helper functions: reflect
+    input: wall_1side_list inout_strands inout_strands_index p
+    output: wall_1side_list
+    """
+    if len(wall_1side_list)==((p-1)/2)+1: # base case
+        return wall_1side_list #return 
     else:
         if inout_strands_index == 0:
-            #print("inside outgoing understrand color case")
-            wall_color = wall_1side_list[-1]
-            #print("wall_color")
-            #print(wall_color)
-            #print("wall_1side_list")
-            #print(wall_1side_list)
-            #print("reflected color")
-            #print(reflect(wall_color,inout_strands[inout_strands_index],p))
+            wall_color = wall_1side_list[-1] # bottom universe at crossing
+            print("wall color is")
+            print(wall_color)
             wall_1side_list.append(reflect(wall_color,inout_strands[inout_strands_index],p))#,inout_strands, 1, p)
-            #print(wall_1side_list)
             return reflect_list( wall_1side_list, inout_strands , 1, p)
         else:
             wall_color = wall_1side_list[-1]
             wall_1side_list.append(reflect(wall_color,inout_strands[inout_strands_index],p))#,inout_strands, 1, p)
             return reflect_list( wall_1side_list, inout_strands , 0, p)
 
-# print(reflect_list([0], [1,2], 0, 3))
-# print(reflect_list([0], [1,2], 1, 3))
-# print(reflect_list([1], [2,0], 0, 3))
-# print(reflect_list([1], [2,0], 1, 3))
-# print(reflect_list([2], [0,1], 0, 3))
-# print(reflect_list([2], [0,1], 1, 3))
-# print(reflect_list([4], [3,0], 0, 5)) #front
-# print(reflect_list([4], [3,0], 1, 5)) #back
-# print(reflect_list([2], [0,4], 0, 5)) #front
-# print(reflect_list([2], [0,4], 1, 5)) #back
-# print(reflect_list([2], [0,1], 0, 5))
-# print(reflect_list([2], [0,1], 1, 5))
-
-
-#want to turn colorlist [3, 0, 4, 2] into
-#incomingoutgoing = [[3,0],[0,4],[4,2],[2,3]]
 def incoming_outgoing_list(color_list):
+    """
+    input:
+    output:
+    """
     incomingoutgoing = []
     for i in range(len(color_list)):
         incomingoutgoing.append([color_list[i],color_list[(i+1)%len(color_list)]])
     return incomingoutgoing
 
-#print(incoming_outgoing_list(color_4_1))
 
 def wall_colors( overstrand_list , color_list , p ):
-    """ co
-    we want input to be
+    """
+    input: overstrand_list, color_list, p an odd number
 
-    overstrand_list, color_list, p an odd number
+    output: all of the walls in all crossings of the knot
 
-    and then the output will be TWO lists of numbers
-
-    one of the lists will be [[[],[]],[[],[]],...,[[],[]]]
+    one of the lists will be [ [ [],[] ],[ [],[] ],...,[ [],[] ] ]
     [[[crossing 0 universe of the top on incoming understand, universe of the one below,...]
       [crossing 0 universe of the top on outgoing understand, universe of the one below,...]]
      [["      " 1 "                                        ", "                       ",...]]
@@ -116,30 +81,60 @@ def wall_colors( overstrand_list , color_list , p ):
     """
     wall_color_lists = []
     incomingoutgoing_list = incoming_outgoing_list(color_list)
-    for crossing in range(len(overstrand_list)):
-        if crossing+1 != len(overstrand_list):
-            wall_color_lists.append([reflect_list( [color_list[overstrand_list[crossing]]] , incomingoutgoing_list[crossing] , 0 , p ), reflect_list( [color_list[overstrand_list[crossing]]] , incomingoutgoing_list[crossing] , 1 , p )])
-        else: #homogeneous crossing
-            wall_color_lists.append
-
+    for crossing in range(len(overstrand_list)):#if the color of the overstrand does not match the color of the incoming understrand
+        if color_list[overstrand_list[crossing]] != color_list[crossing]: #heterogenous
+            heterogenous_wall_list = []
+            heterogenous_wall_list.append(reflect_list( [color_list[overstrand_list[crossing]]] , incomingoutgoing_list[crossing] , 0 , p ))
+            heterogenous_wall_list.append(reflect_list( [color_list[overstrand_list[crossing]]] , incomingoutgoing_list[crossing] , 1 , p ))
+            wall_color_lists.append(heterogenous_wall_list)
+        else: #homogeneous crossing, just want to refect all the colors except the incoming understrand
+            homogeneous_wall_list = []
+            homogeneous_color = color_list[overstrand_list[crossing]]
+            colors = range(0,p).remove(homogeneous_color)
+            homogeneous_wall_list.append(colors)
+            homogeneous_wall_list.append([reflect(color, homogeneous_color, p) for color in colors])
+            wall_color_lists.append(homogeneous_wall_list)
     return wall_color_lists
 
-print(wall_colors(overstrand_5_2,color_5_2,7))
+#print(wall_colors(overstrand_5_2,color_5_2,7))
 
-#
-#
+# homogeneous_wall_list = []
+# p = 7
+# homogeneous_color = 2
+# colors = range(0,p)
+# colors.remove(homogeneous_color)
+# print(colors)
+# homogeneous_wall_list.append(colors)
+# homogeneous_wall_list.append([reflect(color, homogeneous_color, p) for color in colors])
+# print(homogeneous_wall_list)
+
 #wall_colors_8_5 = [[[2,4,5,1], [2, 7, 6, 3]]]#, [[6, 3, 5, 4], [6, 2, 7, 1]], [[3, 5, 6, 2], [3, 1, 7, 4]], [[1, 3, 4, 7], [1, 6, 5, 2]], [[1, 6, 5, 2], [1, 3, 4, 7]], [[4, 0, 5, 6], [4, 1, 3, 2]], [[7, 5, 4, 1], [7, 2, 3, 6]], [[2, 7, 6, 3], [2, 4, 5, 1]]]
-#where_list_8_5 =  [[[1,0,0,1],[0,1,0,0]]]
+#where_list_8_5 =  [[[1,0,0,2],[0,3,0,0]]]
 #print(wall_colors(overstrand_8_5,color_8_5,7))
 #[[2,4,5,1],[2,0,6,3]] [[1,0,0,1],[0,1,0,0]]
 
 def pass_through(where_list):
+    """
+    if you intialize with a list of lists where_lists where a 1-3 represents
+    the location of the numbered men
+    this list "passes the men through to the other side
+    but in reality it just switches the order of the two lists returns a new
+    list with the desired order
+    input:
+    output:
+    """
     pass_where_list = []
     pass_where_list.append(where_list[1])
     pass_where_list.append(where_list[0])
     return pass_where_list
 
+#print(pass_through(where_list_8_5))
+
 def colors_out(where_list,wall_colors):
+    """
+    input:
+    output:
+    """
     colors_out_listoflists = []
     for k in range(len(wall_colors[0])):
         print(where_list[0][k])
