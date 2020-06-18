105a76b5e391e911ecfcef0cf470d3fe3d9121f6
Jack Kendrick <jkendrick@smith.edu>
2020-06-18 12:31:02 +0100

    New method for getting coefficients from matrix

--- matrix_to_dln.py
+++ matrix_to_dln.py
@@ -1,56 +1,31 @@
 import colour_overstrand_to_index2lists as i2l
 import dihedral_linking_five as dl5
+from sympy import Matrix
 
 
-def rrematrix_to_dict(matrix):
-    n = len(matrix)
+def rrematrix_to_dict(rre_matrix):
+    matrix = rre_matrix[0]
+    pivots = list(rre_matrix[1])
+
+    n = len(matrix.col(0))
     num_crossings = n // 2
     coeff_dict = {}
 
     for i in range(n):
         coeff_dict[((i // num_crossings) + 1, i % num_crossings)] = 0
 
-    pivots = []
-
-    # Loop through each of the rows
-    for i in range(n):
-        # set a flag for when a pivot is in the row.
-        pivot_found = False
-
-        # Loop through each of the columns within the row
-        for j in range(len(matrix[i])):
-            # If we haven't found a pivot yet, keep going further right
-            if not pivot_found:
-                # If the value of this entry isn't 0, it's the pivot!
-                if matrix[i][j] != 0:
-                    # Add the location of the entry to our list pivots.
-                    pivot_location = j
-                    pivots.append(pivot_location)
-                    pivot_found = True
-
     for i in range(len(pivots)):
-        row = matrix[i]
-        for column in range(len(row)):
-            if column in pivots:
-                coeff_dict[((column // num_crossings) + 1,
-                            column % num_crossings)] = row[-1]
+        row = matrix.row(i)
+        coeff_dict[((pivots[i] // num_crossings) + 1, pivots[i] % num_crossings)] = row[-1]
 
     return coeff_dict
 
 
-def matrix_to_dln(colourlist, overstrandlist, signlist, p):
+def matrix_to_dln(colourlist, overstrandlist, signlist, coeff_dict, p):
 
     universes_list = i2l.universe_lists(colourlist, overstrandlist, p)
     wheres = i2l.where_lists(colourlist, overstrandlist, p)
     vert_order = i2l.vertical_order(colourlist, overstrandlist, p)
-    coeff_dict = {(1, 1): 1,
-                  (1, 3): -1,
-                  (1, 0): 0,
-                  (1, 2): 0,
-                  (2, 0): 0,
-                  (2, 1): 0,
-                  (2, 2): 0,
-                  (2, 3): 0}
     dlns = []
 
     for i in range(len(wheres)):  # Go through each of the where lists
@@ -89,3 +64,15 @@ def matrix_to_dln(colourlist, overstrandlist, signlist, p):
         dlns.append(dln)
 
     return dlns
+
+
+coeffs = rrematrix_to_dict((Matrix([[1, 0, 0, 0, 1, 0, 1, -1, 0],
+                                    [0, 1, 0, 0, 1, 0, 0, -1, 1],
+                                    [0, 0, 1, 0, 1, -1, 1, 0, 0],
+                                    [0, 0, 0, 1, 0, -1, 1, 0, -1],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0]]), (0, 1, 2, 3)))
+
+print(matrix_to_dln([3, 5, 4, 2], [2, 3, 0, 1], [-1, 1, -1, 1], coeffs, 5))
