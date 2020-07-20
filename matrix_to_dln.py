e4ac6ed99266a37a90f35f26a3f7fa142062c94a
Jack Kendrick <jkendrick@smith.edu>
2020-07-20 11:45:37 +0100

    new function to go straight from braid notation to dln

--- matrix_to_dln.py
+++ matrix_to_dln.py
@@ -1,14 +1,19 @@
 import colour_overstrand_to_index2lists as i2l
 import dihedral_linking_five as dln5
+import braid_to_gauss
+import build_matrix
+import gauss_to_overstrand
+import rrematrix_to_colourlist
 from sympy import Matrix
 
 
-def rrematrix_to_dict(rre_matrix, k):
+def rrematrix_to_dict(rre_matrix, p, k):
     matrix = rre_matrix[0]
     pivots = list(rre_matrix[1])
 
     n = len(matrix.col(0))
-    num_crossings = n // 2
+    num_crossings = n // ((p - 1) // 2)
+    # print("n = ", num_crossings)
     coeff_dict = {}
 
     for i in range(n):
@@ -91,23 +96,59 @@ def matrix_to_dln(colourlist, overstrandlist, signlist, coeff_dict, p, k):
     return dlns
 
 
-# Example: 7_4
-coeffs = rrematrix_to_dict((Matrix([
-                                   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 1, -1, 0, 0, 2],
-                                   [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, -1, 0, 0, 1],
-                                   [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, -1, 0, -1, 2],
-                                   [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, -1, 1],
-                                   [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, -1, 0, -1, 2],
-                                   [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, -1, 1, 0, 0, -2],
-                                   [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, -1],
-                                   [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, -1, 0, -1, 0, 0, 1],
-                                   [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, -2],
-                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, -1, 1, 0, 1, -3],
-                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, -1, 0, 0, 0, -2],
-                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
-                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]), (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 14)), 0)
+def braid_to_dln(braid, p):
+    gauss = braid_to_gauss.BraidToGauss(braid)
 
-print(matrix_to_dln([5, 4, 3, 2, 3, 5, 1, 1], [3, 6, 5, 0, 1, 2, 6, 4], [1, 1, 1, 1, 1, 1, 1, 1], coeffs, 5, 0))
+    signlist = braid_to_gauss.BraidToSigns(braid)
+    overstrandlist = gauss_to_overstrand.create_overstrand_list(gauss)
+    colourlists = rrematrix_to_colourlist.overstrand_to_colourlist(overstrandlist, p)
+    colourlist = colourlists[0]
+
+    if len(signlist) % 2 == 1:
+        signlist.append(1)
+        overstrandlist.append(len(overstrandlist))
+        colourlist.append(colourlist[0])
+
+    # print(signlist, overstrandlist, colourlist)
+
+    mat = build_matrix.create_matrix(overstrandlist, colourlist, signlist, p, 0)
+    coeffs = rrematrix_to_dict(mat, p, 0)
+    # print(coeffs)
+
+    dlns = matrix_to_dln(colourlist, overstrandlist, signlist, coeffs, p, 0)
+    return dlns
+
+# Example: 7_4
+# coeffs = rrematrix_to_dict((Matrix([
+#                                    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 1, -1, 0, 0, 2],
+#                                    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, -1, 0, 0, 1],
+#                                    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, -1, 0, -1, 2],
+#                                    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, -1, 1],
+#                                    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, -1, 0, -1, 2],
+#                                    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, -1, 1, 0, 0, -2],
+#                                    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, -1],
+#                                    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, -1, 0, -1, 0, 0, 1],
+#                                    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, -2],
+#                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, -1, 1, 0, 1, -3],
+#                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, -1, 0, 0, 0, -2],
+#                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+#                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]), (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 14)), 0)
+#
+# print(matrix_to_dln([5, 4, 3, 2, 3, 5, 1, 1], [3, 6, 5, 0, 1, 2, 6, 4], [1, 1, 1, 1, 1, 1, 1, 1], coeffs, 5, 0))
+
+
+# Example: 4_1
+# coeffs = rrematrix_to_dict((Matrix([[1, 0, 0, 0, 0, 1, -1, 0, 1],
+#                                     [0, 1, 0, 0, 0, 1, -1, 1, 0],
+#                                     [0, 0, 1, 0, -1, 0, 0, 1, -1],
+#                                     [0, 0, 0, 1, -1, 1, 0, 1, 0],
+#                                     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+#                                     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+#                                     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+#                                     [0, 0, 0, 0, 0, 0, 0, 0, 0]]), (0, 1, 2, 3)), 5, 0)
+# print(matrix_to_dln([4, 3, 5, 1], [3, 0, 1, 2], [1, -1, 1, -1], coeffs, 5, 0))
+
+# print(braid_to_dln([1, 1, 1, 1, 1], 5))
