238320da80919fc7fef048c9fd26c25002a30b0d
Jack Kendrick <jkendrick@smith.edu>
2020-07-31 12:33:27 +0100

    Removed check of whether linking numbers agree with the formula

--- matrix_to_dln.py
+++ matrix_to_dln.py
@@ -130,9 +130,9 @@ def matrix_to_dln(colourlist, overstrandlist, signlist, coeff_dict, p, k):
                             epsilon1 = -1
                         # print("adding wall", (over_index, overstrandlist[i]))
                         # if epsilon1 * signlist[i] == -1:
-                        #     # print("right")
+                            # print("right")
                         # else:
-                        #     # print("left")
+                            # print("left")
                         dln -= epsilon1 * coeff_dict[(over_index, overstrandlist[i])]
 
                         if k == over_index:
@@ -143,7 +143,7 @@ def matrix_to_dln(colourlist, overstrandlist, signlist, coeff_dict, p, k):
     return dlns
 
 
-def errors(complete_dln):
+def errors(complete_dln, p):
 
     error_list = []
     for i in range(len(complete_dln)):
@@ -157,12 +157,12 @@ def errors(complete_dln):
                         error_message = str(j) + ", " + str(k) + " not equal to " + str(k) + ", " + str(j)
                         error_list.append(error_message)
 
-                if j != 0 and k != 0 and k != j:
-                    if "x" not in [dln_mat[j][k], dln_mat[0][j], dln_mat[0][k]]:
-                        if dln_mat[j][k] != (dln_mat[0][j] + dln_mat[0][k]) / 2:
-                            if str(k) + ", " + str(j) + " not equal to (0, " + str(k) + " + 0, " + str(j) + ")/ 2" not in error_list:
-                                error_message = str(j) + ", " + str(k) + " not equal to (0, " + str(j) + " + 0, " + str(k) + ")/ 2"
-                                error_list.append(error_message)
+                # if j != 0 and k != 0 and k != j:
+                #     if "x" not in [dln_mat[j][k], dln_mat[0][j], dln_mat[0][k]]:
+                #         if dln_mat[j][k] != (dln_mat[0][min(j + k, p - j - k)] + dln_mat[0][abs(j - k)]) / 2:
+                #             if str(k) + ", " + str(j) + " not equal to (0, " + str(k) + " + 0, " + str(j) + ")/ 2" not in error_list:
+                #                 error_message = str(j) + ", " + str(k) + " not equal to (0, " + str(j) + " + 0, " + str(k) + ")/ 2"
+                #                 error_list.append(error_message)
 
     return error_list
 
@@ -198,7 +198,7 @@ def braid_to_dln(braid, p):
             dln_mat.append(dlns)
         complete_dln.append(dln_mat)
 
-    error_list = errors(complete_dln)
+    error_list = errors(complete_dln, p)
 
     if error_list == []:
         return complete_dln
@@ -246,6 +246,6 @@ def gauss_to_dln(gauss, p):
         return ["Error:", error_list]
 
 
-complete_dln = braid_to_dln([1, 1, 1, 1, 1, 1, 1], 7)
+complete_dln = braid_to_dln([1, 1, 1, 2, -1, 2], 7)
 for dln in complete_dln:
     print(dln)
